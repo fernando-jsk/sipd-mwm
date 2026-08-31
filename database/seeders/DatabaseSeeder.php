3a9e3a533ba7de3b14264c3e26bd0fc987824995
Fernando Yohanis <fy@ptjsk.co.id>
2026-08-31 10:06:55 +0800

    feat(seeders): add ReceiptTypeSeeder for receipt categories and subcategories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             RoleAndUserSeeder::class,
             AccountCodeSeeder::class,
             SettingSeeder::class,
+            ReceiptTypeSeeder::class,
         ]);
     }
 }
